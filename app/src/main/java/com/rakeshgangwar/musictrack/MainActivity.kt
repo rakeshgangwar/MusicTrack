af5213a60fa5774a7e21a4dcdd5d1bc69bd2a5a7
Rakesh Gangwar <mail@rakeshgangwar.com>
2017-08-01 02:26:20 +0530

    initial version of listview added to populate song history

--- app/src/main/java/com/rakeshgangwar/musictrack/MainActivity.kt
+++ app/src/main/java/com/rakeshgangwar/musictrack/MainActivity.kt
@@ -15,21 +15,34 @@ import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.Toast
 import com.google.firebase.database.DatabaseError
 import android.R.attr.author
+import android.R.attr.data
+import android.content.Context
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.SimpleCursorAdapter
 import com.google.firebase.database.DataSnapshot
+import java.nio.file.Files.size
+
+
 
 
 
 
 class MainActivity : AppCompatActivity() {
 
+    class songInfo(var timestamp: Long, var trackName: String, var trackAlbum: String, var  trackArtist: String, var trackID: String)
+
     val mAuth:FirebaseAuth = FirebaseAuth.getInstance()
     lateinit var dbReference:DatabaseReference
+    var songList: ArrayList<String> = ArrayList()
+    lateinit var songAdapter:ArrayAdapter<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         displayName.setText("Hello, "+mAuth.currentUser?.displayName)
+        songAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1, songList)
+        songListView.adapter = songAdapter
 
         signoutButton.setOnClickListener(View.OnClickListener {
             mAuth.signOut()
@@ -46,6 +59,18 @@ class MainActivity : AppCompatActivity() {
 
         val postListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val dataSet:Map<String, Map<String, String>> = dataSnapshot.value as Map<String, Map<String, String>>
+                songList.clear()
+
+                val keySet:Set<String> = dataSet.keys
+                val keySetIterator:Iterator<String> = keySet.iterator()
+
+                while (keySetIterator.hasNext()){
+                    var key:String = keySetIterator.next()
+                    songList.add(dataSet.get(key)!!.get("trackName")!!)
+                }
+
+                songAdapter.notifyDataSetChanged()
                 Log.v("Database", "loadPost:onDataChange")
             }
 
